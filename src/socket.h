58c30f174e730fabbc716dccc9379ac60b195977
Gnarwhal <gnarlynarwhal8@gmail.com>
2020-01-17 11:14:34 -0500

    Added windows socket compatibility header

---
+++ src/socket.h
@@ -0,0 +1,48 @@
+#ifndef SABAKURAI_SOCKET
+#define SABAKURAI_SOCKET
+
+#ifdef _WIN32
+	#ifdef _WIN32_WINNT
+		#define _WIN32_WINNT 0x0501
+	#endif
+
+	typedef Socket SOCKET;
+
+	int sockInit() {
+		WSADATA wsaData;
+		return WSAStartup(MAKEWORD(1, 1), &wasData);
+	}
+
+	int sockQuit() {
+		return WSACleanup();
+	}
+
+	int closeSocket(Socket socket) {
+		int status = shutdown(socket, SD_BOTH);
+		if (status == 0) {
+			status = closesocket(socket);
+		}
+		return status;
+	}
+#else
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+
+	typedef Socket int;
+
+	#define INVALID_SOCKET -1
+
+	int sockInit() { return 0; }
+	int sockQuit() { return 0; }
+
+	int closeSocket(Socket socket) {
+		int status = shutdown(socket, SHUT_RDWR);
+		if (status == 0) {
+			status = close(socket);
+		}
+		return status;
+	}
+#endif
+
+#endif // SABAKURAI_SOCKET
