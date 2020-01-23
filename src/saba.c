2b4cd8cd3a769b60002acfc60248132fe2f6aac8
c1fr1 <c1fr1.stkcps@gmail.com>
2020-01-23 21:22:19 +0000

    fixed some typos

--- src/saba.c
+++ src/saba.c
@@ -10,7 +10,7 @@ typedef struct Player {
 } Player;
 
 typedef struct GameData {
-	Player Players[MAX_CLIENTS];
+	Player players[MAX_CLIENTS];
 	int numPlayers;
 	int numReady;
 	int currentState;// 0 = waiting for Players to be ready, 1 = ingame
@@ -18,51 +18,58 @@ typedef struct GameData {
 
 ServerState packetRecievedCB(ServerSession* server, int client, void* data, int length) {
 	GameData* session = (GameData*) server->sessionData;
+	printf("debug: parsing message from [%s], message [%s]", session->players[client].name, data);
 	while (length > 0) {
-		if (session->Players[client].name == 0) {//Player announcing name
+		if (session->players[client].name == 0) {//Player announcing name
 			int nameLength = *((int*) data) & 0xFF;
-			session->Players[client].name = malloc(sizeof(char) * (nameLength + 1));
-			memcpy(session->Players[client].name, ((char*)data) + 1, nameLength * sizeof(char));
-			session->Players[client].name[nameLength] = 0;
-			printf("debug: %s (index %d) announced name (message length: %d, itta %d)\n", session->Players[client].name, client, length, nameLength);
+			session->players[client].name = malloc(sizeof(char) * (nameLength + 1));
+			memcpy(session->players[client].name, ((char*)data) + 1, nameLength * sizeof(char));
+			session->players[client].name[nameLength] = 0;
+			printf("debug: [%s] (index %d) announced name (message length: %d, itta %d)\n", session->players[client].name, client, length, nameLength);
 			length -= nameLength + 1;
+			data = ((char*) data) - (nameLength + 1);
 			++session->numPlayers;
 			continue;
 		}
 		if (session->currentState) {
-
+			fprintf(stderr, "error: invalid game state\n");
+			data = ((char*) data) - 1;
+			--length;
 		} else {
 			char msg = *(char*) data;
 			if (msg == 'r') {
-				printf("debug: %s is now ready\n", session->Players[client].name);
-				session->Players[client].progress = 0;
+				printf("debug: %s is now ready\n", session->players[client].name);
+				session->players[client].progress = 0;
 				++session->numReady;
 			} else if (msg = 'u') {
-				printf("debug: %s is no longer ready\n", session->Players[client].name);
-				session->Players[client].progress = -1;
+				printf("debug: %s is no longer ready\n", session->players[client].name);
+				session->players[client].progress = -1;
 				--session->numReady;
 			}
+			data = ((char*) data) - 1;
 			--length;
 		}
 	}
+	printf("debug: done parsing message\n");
 	return SSTATE_NORMAL;
 }
 
 ServerState newConnectionCB(ServerSession* server, int client, struct sockaddr_in adr) {
 	printf("debug: new connection %d\n", client);
 	GameData* session = (GameData*) server->sessionData;
-	session->Players[client].name = 0;
-	session->Players[client].progress = -1;
+	session->players[client].name = 0;
+	session->players[client].progress = -1;
 }
 
 ServerState disconnectCB(ServerSession* server, int client) {
 	GameData* session = (GameData*) server->sessionData;
+	printf("debug: [%s] disconnected\n", session->players[client]);
 	if (session->currentState) {
 
 	} else {
-		if (session->Players[client].name) {
+		if (session->players[client].name) {
 			--session->numPlayers;
-			if (!session->Players[client].progress) {
+			if (!session->players[client].progress) {
 				--session->numReady;
 			}
 		}
@@ -71,8 +78,10 @@ ServerState disconnectCB(ServerSession* server, int client) {
 
 int main(int argc, char** argv) {
 	ServerSession server;
+	GameData sessionData;
 
-	server.sessionData = calloc(MAX_CLIENTS, sizeof(Player));
+	server.sessionData = &sessionData;
+	memset(&sessionData, 0, sizeof(sessionData));
 
 	createServer(&server, MAX_CLIENTS, PORT);
 
