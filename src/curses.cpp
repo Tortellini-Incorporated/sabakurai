5015e621a28d99207f4f6c78c26a0ff3f3b43fac
Gnarwhal <git.aspect893@passmail.net>
2020-01-24 14:58:04 -0500

    Broken mid ncurses refactor stuff

--- src/curses.cpp
+++ src/curses.cpp
@@ -4,6 +4,8 @@
 #include "window.hpp"
 
 int32_t main(int32_t argc, char ** argv) {
+	auto screen = Window::init();
+
 	cbreak();
 	noecho();
 
@@ -12,17 +14,14 @@ int32_t main(int32_t argc, char ** argv) {
 	refresh();
 
 	Window window(0, 0, COLS, LINES);
-	window.box("Hello World!");
 	window.refresh();
 
 	for (auto i = 0; i < 10; ++i) {
 		getch();
 		window.move(0, 1 + i)
-		      .put(ACS_LTEE)
-		      .line_horz(window.width() - 2, ACS_HLINE)
-			  .move(window.width() - 1, 1 + i)
-		      .put(ACS_RTEE)
-		      .refresh();
+		      .print("Hello World! %d", 32)
+			  .add_char(ACS_RTEE)
+			  .refresh();
 	}
 
 	getch();
--- src/window.cpp
+++ src/window.cpp
@@ -1,18 +1,11 @@
 #include "window.hpp"
 
-Window Window::Internal::root = Window();
-
 Window::Window() {
-	initscr();
-	internal.x = 0;
-	internal.y = 0;
+	internal.x      = 0;
+	internal.y      = 0;
 	internal.width  = COLS;
 	internal.height = LINES;
 	internal.window = stdscr;
-}
-
-auto Window::root() -> Window& {
-	return Internal::root;
 }
 
 Window::Window(uint32_t x, uint32_t y, uint32_t width, uint32_t height) :
@@ -31,49 +24,21 @@ auto Window::operator=(Window && window) -> Window& {
 	return *this;
 }
 
-auto Window::box() -> Window& {
-	box(0, 0, internal.width, internal.height);
+auto Window::move(uint32_t x, uint32_t y) -> Window& {
+	wmove(internal.window, y, x);
 	return *this;
 }
 
-auto Window::box(const std::string & name) -> Window& {
-	box(0, 0, internal.width, internal.height);
-	mvwprintw(internal.window, 0, 2, " %s ", name.c_str());
+auto Window::add_char(uint32_t c) -> Window& {
+	waddch(internal.window, c);
 	return *this;
 }
 
-auto Window::box(uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> Window& {
-	mvwaddch(internal.window, y,              x,             ACS_ULCORNER);
-	mvwaddch(internal.window, y + height - 1, x,             ACS_LLCORNER);
-	mvwaddch(internal.window, y,              x + width - 1, ACS_URCORNER);
-	mvwaddch(internal.window, y + height - 1, x + width - 1, ACS_LRCORNER);
-	mvwhline(internal.window, y,              x + 1, ACS_HLINE, width  - 2);
-	mvwhline(internal.window, y + height - 1, x + 1, ACS_HLINE, width  - 2);
-	mvwvline(internal.window, y + 1, x,              ACS_VLINE, height - 2);
-	mvwvline(internal.window, y + 1, x + width - 1,  ACS_VLINE, height - 2);
-	return *this;
-}
-
-auto Window::box(uint32_t x, uint32_t y, uint32_t width, uint32_t height, const std::string & name) -> Window& {
-	box(x, y, width, height);
-	mvwprintw(internal.window, y, x + 2, " %s ", name.c_str());
-	return *this;
-}
-
-auto Window::unbox() -> Window& {
-	unbox(0, 0, internal.width, internal.height);
-	return *this;
-}
-
-auto Window::unbox(uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> Window& {
-	mvwaddch(internal.window, y,              x,             ' ');
-	mvwaddch(internal.window, y + height - 1, x,             ' ');
-	mvwaddch(internal.window, y,              x + width - 1, ' ');
-	mvwaddch(internal.window, y + height - 1, x + width - 1, ' ');
-	mvwhline(internal.window, y,              x + 1, ' ', width  - 2);
-	mvwhline(internal.window, y + height - 1, x + 1, ' ', width  - 2);
-	mvwvline(internal.window, y + 1, x,              ' ', height - 2);
-	mvwvline(internal.window, y + 1, x + width - 1,  ' ', height - 2);
+auto Window::print(const char * format, ...) -> Window& {
+	std::va_list args;
+	va_start(args, format);
+	vwprintw(internal.window, format, args);
+	va_end(args);
 	return *this;
 }
 
@@ -81,26 +46,6 @@ auto Window::clear() -> Window& {
 	for (auto i = uint32_t{ 0 }; i < internal.width; ++i) {
 		mvwvline(internal.window, 0, i, ' ', internal.height);
 	}
-	return *this;
-}
-
-auto Window::move(uint32_t x, uint32_t y) -> Window& {
-	wmove(internal.window, y, x);
-	return *this;
-}
-
-auto Window::put(uint32_t c) -> Window& {
-	waddch(internal.window, c);
-	return *this;
-}
-
-auto Window::line_horz(uint32_t width, uint32_t c) -> Window& {
-	whline(internal.window, c, width);
-	return *this;
-}
-
-auto Window::line_vert(uint32_t height, uint32_t c) -> Window& {
-	wvline(internal.window, c, height);
 	return *this;
 }
 
@@ -125,14 +70,41 @@ auto Window::height() -> uint32_t {
 	return internal.height;
 }
 
+auto Window::window() -> WINDOW* {
+	return internal.window;
+}
+
 Window::~Window() {
-	if (internal.window != stdscr) {
-		clear();
-		refresh();
-		if (internal.window) {
-			delwin(internal.window);
-		}
-	} else {
-		endwin();
+	clear();
+	refresh();
+	if (internal.window) {
+		delwin(internal.window);
 	}
+}
+
+auto RootWindow::getch() -> void {
+	auto ret = c;
+	c = getch();
+	if (c == KEY_RESIZE) {
+		Window::internal.component.rezize();
+		c = getch();
+	}
+	return ret;
+}
+
+RootWindow::RootWindow() {
+	initscr();
+	Window::internal.x      = 0;
+	Window::internal.y      = 0;
+	Window::internal.width  = COLS;
+	Window::internal.height = LINES;
+	Window::internal.window = stdscr;
+}
+
+void RootWindow::block(bool value) -> void {
+	nedelay(Window::internal.window, !value);
+}
+
+RootWindow::~RootWindow() {
+	endwin();
 }
