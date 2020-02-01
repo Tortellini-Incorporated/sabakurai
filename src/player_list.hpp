20cea5890d1ab9b16a0d7cd441c38d10e4cc74f0
Gnarwhal <git.aspect893@passmail.net>
2020-02-01 10:59:08 -0500

    Fixed a bunch of skek with resizing. Added a player list window.

---
+++ src/player_list.hpp
@@ -0,0 +1,35 @@
+#ifndef SABAKURAI_PLAYER_LIST
+#define SABAKURAI_PLAYER_LIST
+
+#include <vector>
+#include <string>
+
+#include "window.hpp"
+
+class PlayerList : public Window {
+	public:
+		struct Player {
+			uint32_t id;
+			uint32_t color;
+			std::string name;
+		};
+
+	protected:
+		virtual auto component_resize() -> void;
+
+		uint32_t offset;
+		std::vector<Player> players;
+
+	public:
+		PlayerList();
+		PlayerList(Window & root, bool dummy);
+		PlayerList(Window & root, uint32_t x, uint32_t y, uint32_t width, uint32_t height);
+		auto add_player(uint32_t id, uint32_t color, std::string name) -> void;
+		auto remove_player(uint32_t id) -> void;
+		virtual auto draw() -> Window&;
+		~PlayerList();	
+};
+
+#define SABAKURAI_PLAYER_LIST_FORWARD
+#endif // SABAKURAI_PLAYER_LIST
+
