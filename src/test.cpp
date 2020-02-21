12a40d728e635885fdd5d880c9693541da30050d
Gnarwhal <gnarlynarwhal8@gmail.com>
2020-02-20 19:58:51 -0500

    Test B

--- src/test.cpp
+++ src/test.cpp
@@ -11,8 +11,6 @@ int main() {
 	root.move(1, 1).horz_line(root.width()  - 2, ACS_HLINE);
 	root.move(1, 2).vert_line(root.height() - 3, ACS_VLINE);
 
-	root.draw().refresh();
-
 	mvaddch(5, 5, ACS_HLINE);
 	mvaddch(6, 6, ACS_VLINE);
 
