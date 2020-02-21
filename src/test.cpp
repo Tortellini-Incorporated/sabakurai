a3827ae8fd649a3d33f401fe30efb193b812ea41
Gnarwhal <git.aspect893@passmail.net>
2020-02-20 19:57:33 -0500

    Test A

--- src/test.cpp
+++ src/test.cpp
@@ -8,10 +8,15 @@ std::ofstream file("debug-test.log");
 int main() {
 	Box root = { "Test 9" };
 
-	root.move(1, 1).horz_line(5, ACS_ULCORNER);
-	root.move(5, 5).vert_line(5, ACS_LLCORNER);
+	root.move(1, 1).horz_line(root.width()  - 2, ACS_HLINE);
+	root.move(1, 2).vert_line(root.height() - 3, ACS_VLINE);
 
 	root.draw().refresh();
+
+	mvaddch(5, 5, ACS_HLINE);
+	mvaddch(6, 6, ACS_VLINE);
+
+	root.refresh();
 
 	root.get_char();
 
