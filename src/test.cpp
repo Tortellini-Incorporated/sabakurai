c1d8032d4740e935e26811008b93beeebaa05649
Gnarwhal <gnarlynarwhal8@gmail.com>
2020-02-21 10:52:18 -0500

    Test 10

--- src/test.cpp
+++ src/test.cpp
@@ -8,8 +8,8 @@ std::ofstream file("debug-test.log");
 int main() {
 	Box root = { "Test F (Test 5++)" };
 
-	root.move(1, 1).horz_line(5, ACS_ULCORNER);
-	root.move(3, 3).vert_line(5, ACS_LLCORNER);
+	root.move(1, 1).horz_line(5, ACS_HLINE);
+	root.move(3, 3).vert_line(5, ACS_VLINE);
 
 	root.refresh();
 
