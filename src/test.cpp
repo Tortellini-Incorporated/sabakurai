1f52e8f64ade0d159d53c145c91b1cc6c6a58824
Gnarwhal <gnarlynarwhal8@gmail.com>
2020-02-21 17:50:59 -0500

    Test 16

--- src/test.cpp
+++ src/test.cpp
@@ -8,6 +8,13 @@ std::ofstream file("debug-test.log");
 int main() {
 	Box root = { "Test F (Test 5++)" };
 
+	for (auto i = 0; i < 20; ++i) {
+		mvhline(i, 0, ACS_HLINE, i + 1);
+	}
+	refresh();
+
+	getch();
+
 	root.draw().refresh();
 
 	root.get_char();
