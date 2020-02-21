362dd3a9dd0a040b93e129289317b0b4bb5a80e1
Gnarwhal <git.aspect893@passmail.net>
2020-02-20 20:03:22 -0500

    Test D

--- src/test.cpp
+++ src/test.cpp
@@ -8,8 +8,8 @@ std::ofstream file("debug-test.log");
 int main() {
 	Box root = { "Test 9" };
 
-	mvhline(2, 1, ACS_HLINE, root.width()  - 2);
-	root.move(1, 1).horz_line(root.width() - 2, ACS_HLINE);
+	mvhline(2, 1, 'C', root.width()  - 2);
+	root.move(1, 1).horz_line(root.width() - 2, 'C');
 
 	mvaddch(5, 5, ACS_HLINE);
 	mvaddch(6, 6, ACS_VLINE);
