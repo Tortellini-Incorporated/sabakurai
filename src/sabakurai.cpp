b20d4639e952147b50d496040e2d2be02e9db23a
Gnarwhal <git.aspect893@passmail.net>
2020-03-03 11:58:30 -0500

    Moved components into a single struct

--- src/sabakurai.cpp
+++ src/sabakurai.cpp
@@ -38,8 +38,25 @@ struct Command {
 	}
 };
 
-void dispatch_command(const std::string & raw, const std::vector<Command> & commands, LogCallback error);
+void dispatch_command(const std::string & raw, const std::vector<Command> & commands, Log & log);
 std::vector<std::string> get_args(int32_t start, const std::string & raw);
+
+struct LobbyState {
+	Box root;
+	Split split;
+	Box title_box;
+	Title title;
+	Box player_box;
+	PlayerList players;
+	Box log_box;
+	Log log;
+	Box command_box;
+	CommandPrompt command;
+	Split split2;
+	Split split3;
+
+	Socket socket;
+};
 
 int32_t main(int32_t argc, char ** argv) {
 	auto ip = std::string();
@@ -62,79 +79,71 @@ int32_t main(int32_t argc, char ** argv) {
 		std::getline(std::cin, username);
 	}
 
-	Box root = { "" };
-	root.block(false);
+	LobbyState lobby = {
+		{ "" },
+
+		{
+			lobby.root,
+			[](uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> uint32_t {
+				return height < 7 ? height : 7;
+			},
+			Split::HORZ
+		},
+
+		{ lobby.root, "" },
+		{ lobby.root, true },
+
+		{ lobby.root, "Players" },
+		{ lobby.root, true },
+
+		{ lobby.root, "Log" },
+		{ lobby.root, true },
+
+		{ lobby.root, "Command" },
+		{ lobby.root, true },
+		{
+			lobby.root,
+			[](uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> uint32_t {
+				return width * 0.8;
+			},
+			Split::VERT
+		},
+		{
+			lobby.root,
+			[&lobby](uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> uint32_t {
+				if (lobby.command.line_count() + 2 > height - (height * (3.0 / 4.0))) {
+					return height * (3.0 / 4.0);
+				} else {
+					return height - lobby.command.line_count() - 2;
+				}
+			},
+			Split::HORZ
+		},
+		{ PF_INET, SOCK_STREAM, IPPROTO_TCP }
+	};
+	lobby.root.block(false);
 
 	cbreak();
 	noecho();
 
-	file << "Wat" << std::endl;
-
-	Split split = {
-		root,
-		[](uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> uint32_t {
-			return height < 7 ? height : 7;
-		},
-		Split::HORZ
-	};
-	file << "Wat2" << std::endl;
-
-	Box titleBox = { root, "" };
-	Title title = { root, true };
-	titleBox.set_child(&title);
-
-	file << "Wat3" << std::endl;
-	Box playerBox = { root, "Players" };
-	PlayerList players = { root, true };
-	playerBox.set_child(&players);
-	file << "Wat4" << std::endl;
-
-	Box logBox = { root, "Log" };
-	Log log = { root, true };
-	log.message("Welcome to sabakurai! For assistance please enter /help");
-	logBox.set_child(&log);
-
-	file << "Wat5" << std::endl;
-	Box commandBox = { root, "Command" };
-	CommandPrompt command = { root, true };
-	file << "Hello there" << std::endl;
-	commandBox.set_child(&command);
-
-	file << "Command prompt inited" << std::endl;
-
-	Split split2 = {
-		root,
-		[](uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> uint32_t {
-			return width * 0.8;
-		},
-		Split::VERT
-	};
-	Split split3 = {
-		root,
-		[&command](uint32_t x, uint32_t y, uint32_t width, uint32_t height) -> uint32_t {
-			if (command.line_count() + 2 > height - (height * (3.0 / 4.0))) {
-				return height * (3.0 / 4.0);
-			} else {
-				return height - command.line_count() - 2;
-			}
-		},
-		Split::HORZ
-	};
-	file << "Setting children :snatch:" << std::endl;
-	split3.set_children(&logBox, &commandBox);
-	split2.set_children(&split3, &playerBox);
-	split.set_children(&titleBox, &split2);
-	root.set_child(&split);
+	lobby.title_box.set_child(&lobby.title);
+	lobby.player_box.set_child(&lobby.players);
+	lobby.log_box.set_child(&lobby.log);
+	lobby.command_box.set_child(&lobby.command);
+	lobby.split3.set_children(&lobby.log_box, &lobby.command_box);
+	lobby.split2.set_children(&lobby.split3, &lobby.player_box);
+	lobby.split.set_children(&lobby.title_box, &lobby.split2);
+	lobby.root.set_child(&lobby.split);
 	
-	file << "Tryna draw" << std::endl;
-	root.draw().refresh();
+	lobby.log.message("Welcome to sabakurai! For assistance please enter /help");
+
+	lobby.root.draw().refresh();
 
 	try {
-		Socket socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 		file << "Socket created successfully" << std::endl;
 
 		file << "Trying to connect to server..." << std::endl;
-		socket.addressInfo({
+		lobby.socket.addressInfo({
 			AF_INET,
 			htons(PORT),
 			inet_addr(ip.c_str())
@@ -142,37 +151,32 @@ int32_t main(int32_t argc, char ** argv) {
 		file << "Connected to server at ip '" << ip << '\'' << std::endl;
 
 		file << "Sending user info..." << std::endl;
-		socket
+		lobby.socket
 			<< uint8_t( username.size() )
 			<< username
 			<< Socket::FLUSH;
 
-		auto my_id = socket.read();
-		auto player_count = socket.read();
+		auto my_id = lobby.socket.read();
+		auto player_count = lobby.socket.read();
 		file << "Id: " << (uint32_t) my_id << ", " << (uint32_t) player_count << std::endl;
 		for (auto i = uint32_t{ 0 }; i < player_count; ++i) {
-			auto id    = uint32_t( socket.read() );
-			auto ready = bool( socket.read() );
+			auto id    = uint32_t( lobby.socket.read() );
+			auto ready = bool( lobby.socket.read() );
 			auto name  = std::string();
-			socket.width(Socket::U8) >> name;
-			players.add_player(id, 0, ready, name);
+			lobby.socket.width(Socket::U8) >> name;
+			lobby.players.add_player(id, 0, ready, name);
 		}
-		auto & self = players.get_player(my_id);
-		players.draw().refresh();
+		auto & self = lobby.players.get_player(my_id);
+		lobby.players.draw().refresh();
 		
 		bool quit = false;
 
 		////// COMMANDS //////
 
-		auto log_callback = [&log](uint32_t color, const std::string & string) -> void {
-			log.message(string);
-			log.draw().refresh();
-		};
-
 		auto commands = std::vector<Command>{
 			Command{
 				"say",
-				[&socket](const std::vector<std::string> & args) -> void {
+				[&lobby](const std::vector<std::string> & args) -> void {
 					auto message = std::string();
 					for (auto i = 0; i < args.size(); ++i) {
 						message.append(args[i]);
@@ -181,7 +185,7 @@ int32_t main(int32_t argc, char ** argv) {
 						}
 					}
 					file << "Say: " << message << std::endl;
-					socket
+					lobby.socket
 						<< uint8_t( 0x05 )
 						<< uint16_t( message.size() )
 						<< message
@@ -196,8 +200,8 @@ int32_t main(int32_t argc, char ** argv) {
 			},
 			Command{
 				"help",
-				[&log](const std::vector<std::string> & args) -> void {
-					log.message(
+				[&lobby](const std::vector<std::string> & args) -> void {
+					lobby.log.message(
 						"Type a message and hit enter to send. Shift + Tab is new line. To enter a command prefix the message with a '/'. To send a message that starts with a '/' use a '//' at the start.\n"
 						"Commands:\n"
 						"- /help          - prints the help menu\n"
@@ -206,20 +210,20 @@ int32_t main(int32_t argc, char ** argv) {
 						"- /ready         - toggle ready status\n"
 						"- /name <string> - changes your name - UNIMPLEMENTED\n"
 					);
-					log.draw().refresh();
+					lobby.log.draw().refresh();
 				}
 			},
 			Command{
 				"ready",
-				[&socket](const std::vector<std::string> & args) -> void {
-					socket
+				[&lobby](const std::vector<std::string> & args) -> void {
+					lobby.socket
 						<< uint8_t( 0 )
 						<< Socket::FLUSH;
 				}
 			},
 			Command{
 				"name",
-				[&self, &socket, &log_callback, &log](const std::vector<std::string> & args) -> void {
+				[&lobby](const std::vector<std::string> & args) -> void {
 					auto name = std::string();
 					for (auto i = 0; i < args.size(); ++i) {
 						name.append(args[i]);
@@ -228,10 +232,10 @@ int32_t main(int32_t argc, char ** argv) {
 						}
 					}
 					if (name.size() > 0xFF) {
-						log_callback(0x00, "Sorry. Max name length is 255.");
-						log.draw().refresh();
+						lobby.log.message(/*0x00, */"Sorry. Max name length is 255.");
+						lobby.log.draw().refresh();
 					} else {
-						socket
+						lobby.socket
 							<< uint8_t( 0x01 )
 							<< uint8_t( name.size() )
 							<< name
@@ -244,21 +248,21 @@ int32_t main(int32_t argc, char ** argv) {
 		file << "Looping..." << std::endl;
 
 		while (!quit) {
-			if (command.update()) {
-				if (command.complete()) {
-					dispatch_command(command.get(), commands, log_callback);
-					command.clear_command();
+			if (lobby.command.update()) {
+				if (lobby.command.complete()) {
+					dispatch_command(lobby.command.get(), commands, lobby.log);
+					lobby.command.clear_command();
 				}
-				if (command.height_change(command.height() + 1)) {
-					split3.component_resize();
-					logBox.draw();
-					commandBox.draw();
-					split3.refresh();
+				if (lobby.command.height_change(lobby.command.height() + 1)) {
+					lobby.split3.component_resize();
+					lobby.log_box.draw();
+					lobby.command_box.draw();
+					lobby.split3.refresh();
 				} else {
-					command.draw().refresh();
+					lobby.command.draw().refresh();
 				}
 			}
-			split3.refresh();
+			lobby.split3.refresh();
 			constexpr static uint8_t
 				CONNECT       = 0x00,
 				TOGGLE_READY  = 0x01,
@@ -266,40 +270,40 @@ int32_t main(int32_t argc, char ** argv) {
 				START         = 0x03,
 				UPDATE_NAME   = 0x08,
 				RELAY_MESSAGE = 0x09;
-			if (socket.poll(16)) {
-				auto read = socket.read();
+			if (lobby.socket.poll(16)) {
+				auto read = lobby.socket.read();
 				switch (read) {
 					case CONNECT: {
-						auto id   = uint32_t( socket.read() );
+						auto id   = uint32_t( lobby.socket.read() );
 						auto name = std::string();
-						socket.width(Socket::U8) >> name;
+						lobby.socket.width(Socket::U8) >> name;
 						if (id != my_id) {
-							log.message(std::string("Player '").append(name).append("' connected"));
-							players.add_player(id, 0, false, name);
-							players.draw().refresh();
-							log.draw().refresh();
+							lobby.log.message(std::string("Player '").append(name).append("' connected"));
+							lobby.players.add_player(id, 0, false, name);
+							lobby.players.draw().refresh();
+							lobby.log.draw().refresh();
 						}
 						break;
 					}
 					case TOGGLE_READY: {
-						auto id = uint32_t( socket.read() );
-						auto & player = players.get_player(id);
+						auto id = uint32_t( lobby.socket.read() );
+						auto & player = lobby.players.get_player(id);
 						player.ready = !player.ready;
 						if (player.ready) {
-							log.message(std::string("Player '").append(player.name).append("' is ready"));
+							lobby.log.message(std::string("Player '").append(player.name).append("' is ready"));
 						} else {
-							log.message(std::string("Player '").append(player.name).append("' is no longer ready"));
+							lobby.log.message(std::string("Player '").append(player.name).append("' is no longer ready"));
 						}
-						players.draw().refresh();
-						log.draw().refresh();
+						lobby.players.draw().refresh();
+						lobby.log.draw().refresh();
 						break;
 					}
 					case DISCONNECT: {
-						auto id = uint32_t( socket.read() );
-						log.message(std::string("Player '").append(players.get_player(id).name).append("' disconnected"));
-						players.remove_player(id);
-						players.draw().refresh();
-						log.draw().refresh();
+						auto id = uint32_t( lobby.socket.read() );
+						lobby.log.message(std::string("Player '").append(lobby.players.get_player(id).name).append("' disconnected"));
+						lobby.players.remove_player(id);
+						lobby.players.draw().refresh();
+						lobby.log.draw().refresh();
 						break;
 					}
 					case START: {
@@ -307,20 +311,20 @@ int32_t main(int32_t argc, char ** argv) {
 						break;
 					}
 					case UPDATE_NAME: {
-						auto id = uint32_t( socket.read() );
-						auto & player = players.get_player(id);
-						socket.width(Socket::U8) >> player.name;
+						auto id = uint32_t( lobby.socket.read() );
+						auto & player = lobby.players.get_player(id);
+						lobby.socket.width(Socket::U8) >> player.name;
 						file << "New Name: " << player.name << std::endl;
-						players.draw().refresh();
+						lobby.players.draw().refresh();
 						break;
 					}
 					case RELAY_MESSAGE: {
-						auto id = uint32_t( socket.read() );
-						auto & player = players.get_player(id);
+						auto id = uint32_t( lobby.socket.read() );
+						auto & player = lobby.players.get_player(id);
 						auto message = std::string();
-						socket.width(Socket::U16) >> message;
-						log.message(player.name, message);
-						log.draw().refresh();
+						lobby.socket.width(Socket::U16) >> message;
+						lobby.log.message(player.name, message);
+						lobby.log.draw().refresh();
 						break;
 					}
 					default: {
@@ -328,7 +332,7 @@ int32_t main(int32_t argc, char ** argv) {
 					}
 				}
 			}
-			command.move_cursor();
+			lobby.command.move_cursor();
 		}
 	} catch (const std::string & message) {
 		file << "[ERROR] " << message << std::endl;
@@ -338,7 +342,7 @@ int32_t main(int32_t argc, char ** argv) {
 	return 0;
 }
 
-void dispatch_command(const std::string & raw, const std::vector<Command> & commands, LogCallback error) {
+void dispatch_command(const std::string & raw, const std::vector<Command> & commands, Log & error) {
 	file << "Dispatching: " << raw << std::endl;
 	if (raw.size() > 0) {
 		if (raw[0] != '/') {
@@ -359,7 +363,7 @@ void dispatch_command(const std::string & raw, const std::vector<Command> & comm
 			
 			if (i == commands.size()) {
 				auto error_message = std::string("Undefined command '").append(raw, 0, end).append(1, '\'');
-				error(0x00, error_message);
+				error.message(/*0x00, */error_message);
 			}
 		}
 	}
